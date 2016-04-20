4afa84184e8bc05f6a0cf864f26963a4f477817a
Igor <sapegin@private-it.ru>
2016-04-20 19:29:41 +0600

    run only web\Application

--- src/WidgetBootstrap.php
+++ src/WidgetBootstrap.php
@@ -4,6 +4,7 @@ namespace PrivateIT\widgets\bootstrap;
 
 use yii\base\BootstrapInterface;
 use yii\helpers\ArrayHelper;
+use yii\web\Application;
 
 class WidgetBootstrap implements BootstrapInterface
 {
@@ -11,13 +12,15 @@ class WidgetBootstrap implements BootstrapInterface
      * @param \yii\base\Application $app
      */
     public function bootstrap($app) {
-        $request = $app->request;
-        if ($request->isPost) {
-            $widget = $request->post('widget');
-            if ($widget) {
-                $cls = ArrayHelper::getValue($widget, 'cls');
-                $widgetId = ArrayHelper::getValue($widget, 'id');
-                call_user_func([$cls, 'bootstrap'], $app, $widgetId);
+        if ($app instanceof Application) {
+            $request = $app->request;
+            if ($request->isPost) {
+                $widget = $request->post('widget');
+                if ($widget) {
+                    $cls = ArrayHelper::getValue($widget, 'cls');
+                    $widgetId = ArrayHelper::getValue($widget, 'id');
+                    call_user_func([$cls, 'bootstrap'], $app, $widgetId);
+                }
             }
         }
     }
