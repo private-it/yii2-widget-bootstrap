8c8615510a79b713352acd341c10301d11f6c61e
Rendol <sapegin.in@gmail.com>
2016-04-25 05:17:17 +0600

    Add method for init js

--- src/AbstractWidget.php
+++ src/AbstractWidget.php
@@ -6,6 +6,7 @@ namespace PrivateIT\widgets\bootstrap;
 use yii\base\Widget;
 use yii\helpers\Html;
 use yii\helpers\Inflector;
+use yii\helpers\Json;
 use yii\web\Application;
 
 abstract class AbstractWidget extends Widget
@@ -14,6 +15,10 @@ abstract class AbstractWidget extends Widget
      * @var array
      */
     public $options;
+    /**
+     * @var array
+     */
+    public $clientOptions = [];
 
     /**
      * @param Application $app
@@ -32,6 +37,31 @@ abstract class AbstractWidget extends Widget
             $this->options['id'] = $this->getId();
         }
         Html::addCssClass($this->options, $this->getWidgetCssClass());
+    }
+
+    /**
+     * Initializes the view.
+     */
+    public function initJs()
+    {
+        $id = $this->options['id'];
+        $options = $this->getClientOptions();
+        $options = sizeof($options) ? Json::htmlEncode($options) : '';
+        $this->getView()->registerJs('jQuery(\'#' . $id . '\').' . $this->getWidgetJsClass() . '(' . $options . ');');
+    }
+
+    /**
+     * Generate css class
+     *
+     * @param string $cls
+     * @return string
+     */
+    public function getWidgetJsClass($cls = null)
+    {
+        if (null === $cls) {
+            $cls = get_class($this);
+        }
+        return Inflector::variablize(basename(str_replace('\\', DIRECTORY_SEPARATOR, $cls)));
     }
 
     /**
@@ -57,4 +87,13 @@ abstract class AbstractWidget extends Widget
     }
 
     abstract public function getContent();
+
+    /**
+     * Returns the options for the grid view JS widget.
+     * @return array the options
+     */
+    public function getClientOptions()
+    {
+        return $this->clientOptions;
+    }
 }
